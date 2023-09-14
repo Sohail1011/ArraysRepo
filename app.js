0f80db5432265b10f6e86159be2627e26a2500f2
Sohail Iran <aratavalga@gmail.com>
2023-09-13 21:01:36 -0500

    arrays

---
+++ app.js
@@ -0,0 +1,7 @@
+const comidas = ['Sushi', 'Pollo con naranja', 'Gimbak', 'Lasaña', 'Lentejas', 'Pizza', 'Hamburguesa'];
+
+const voucher = [20.5, true];
+
+const persona = ['Carlos', 'Urtedad', 20, ['cocinero', 'asistente']];
+
+console.log(comidas);
