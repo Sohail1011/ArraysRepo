9b9f11c8b7bbdc1868af5bd562cda2a320fbd998
Sohail Iran <aratavalga@gmail.com>
2023-09-13 21:15:50 -0500

    añadidos

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+
+// Los Arrays
 const comidas = ['Sushi', 'Pollo con naranja', 'Gimbak', 'Lasaña', 'Lentejas', 'Pizza', 'Hamburguesa'];
 
+// Tipos de datos que admite los arrays
 const voucher = [20.5, true];
 
+// Método de iteración
+for (let i = 0; i < comidas.length; i++) {
+    console.log(comidas[i]);
+}
+
+// Arrays bidimensionales
 const persona = ['Carlos', 'Urtedad', 20, ['cocinero', 'asistente']];
 
-console.log(comidas);
+const clientes = [
+    { id: 1, name: 'Sara' },
+    { id: 2, name: 'Tom' },
+    { id: 3, name: 'Carl' },
+    { id: 4, name: 'Juana' },
+];
